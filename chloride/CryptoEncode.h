5030c90c43d2c2c0c7c59c73c6576c282725700b
Guy Vreuls <guyvreuls@gmail.com>
2015-12-22 21:07:21 +0100

    Added license.
    Signed-off-by: Guy Vreuls <guyvreuls@gmail.com>

--- chloride/CryptoEncode.h
+++ chloride/CryptoEncode.h
@@ -3,6 +3,23 @@
 **
 **  Created on: Dec 13, 2015
 **      Author: gv
+**
+** This file is part of libchloride.
+** Copyright (C) 2015 Guy Vreuls
+**
+** Libchloride is free software: you can redistribute it and/or modify
+** it under the terms of the GNU Lesser General Public License as
+** published by the Free Software Foundation, either version 2.1 of
+** the License, or (at your option) any later version.
+**
+** Libchloride is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU Lesser General Public License for more details.
+**
+** You should have received a copy of the Lesser GNU General Public
+** License along with libchloride.  If not, see
+** <http://www.gnu.org/licenses/>.
 */
 
 #ifndef CHLORIDE_CRYPTOENCODE_H_
